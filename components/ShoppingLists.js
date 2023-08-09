e962de4594ec1a8fa00e4550cabc196f828c5964
Bryant Ortega <chase@monarca.systems>
2023-08-09 13:49:43 -0700

    updated ShoppingLists.js

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View, FlatList, Text } from "react-native";
+import {
+    StyleSheet, View, FlatList, Text,
+    TextInput, KeyboardAvoidingView,
+    TouchableOpacity
+  } from 'react-native';
 
 import { collection, getDocs } from "firebase/firestore";
 
 const ShoppingLists = ({ db }) => {
     const [lists, setLists] = useState([]);
+    const [listName, setListName] = useState("");
+    const [item1, setItem1] = useState("");
+    const [item2, setItem2] = useState("");
 
     const fetchShoppingLists = async () => {
         const listsDocuments = await getDocs(collection(db, "shoppinglists"));
@@ -21,22 +28,95 @@ const ShoppingLists = ({ db }) => {
 
     return (
         <View style={styles.container}>
-            <FlatList
-                data={lists}
-                renderItem={({ item }) => (
-                    <Text>
-                        {item.name}: {item.items.join(", ")}
-                    </Text>
-                )}
+          <FlatList
+            style={styles.listsContainer}
+            data={lists}
+            renderItem={({ item }) =>
+              <View style={styles.listItem}>
+                <Text >{item.name}: {item.items.join(", ")}</Text>
+              </View>
+            }
+          />
+          <View style={styles.listForm}>
+            <TextInput
+              style={styles.listName}
+              placeholder="List Name"
+              value={listName}
+              onChangeText={setListName}
             />
+            <TextInput
+              style={styles.item}
+              placeholder="Item #1"
+              value={item1}
+              onChangeText={setItem1}
+            />
+            <TextInput
+              style={styles.item}
+              placeholder="Item #2"
+              value={item2}
+              onChangeText={setItem2}
+            />
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => { }}
+            >
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+          </View>
+          {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
         </View>
-    );
+      )
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+      flex: 1
     },
-});
+    listItem: {
+      height: 70,
+      justifyContent: "center",
+      paddingHorizontal: 30,
+      borderBottomWidth: 1,
+      borderBottomColor: "#AAA",
+      flex: 1,
+      flexGrow: 1
+    },
+    listForm: {
+      flexBasis: 275,
+      flex: 0,
+      margin: 15,
+      padding: 15,
+      backgroundColor: "#CCC"
+    },
+    listName: {
+      height: 50,
+      padding: 15,
+      fontWeight: "600",
+      marginRight: 50,
+      marginBottom: 15,
+      borderColor: "#555",
+      borderWidth: 2
+    },
+    item: {
+      height: 50,
+      padding: 15,
+      marginLeft: 50,
+      marginBottom: 15,
+      borderColor: "#555",
+      borderWidth: 2
+    },
+    addButton: {
+      justifyContent: "center",
+      alignItems: "center",
+      height: 50,
+      backgroundColor: "#000",
+      color: "#FFF"
+    },
+    addButtonText: {
+      color: "#FFF",
+      fontWeight: "600",
+      fontSize: 20
+    }
+  });
 
 export default ShoppingLists;
